1224e1470a51ce8463998c67ea4f7ec318684dce
Diego Brito <diego.brito@f1rst.com.br>
2024-02-04 21:41:21 -0300

    atualização para de arquivos terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,49 @@
-variable "region" {
-  default     = "us-east-1"
-  description = "AWS region"
-}
-
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "lanchonete-eks-${random_string.suffix.result}"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "lanchonete-vpc"
+  cidr = "10.0.0.0/16"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+module "subnets" {
+  source = "terraform-aws=modules/subnet/aws"
+  availability_zones = ["us-east-1a", "us-east-1b","us-east-1c"]
+  vpc_id = module.vpc.vpc_id
+}
+
+module "security_groups" {
+  source = "terraform-aws=modules/security-group/aws"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "lanchonete_dbb_terraform_state_bucket"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "aws_s3_bucket.terraform_state.bucket"
+    key = "enviroments/mysql-cliente/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "terraform_lock"
+  }
+}
+
+module "rds_mysql_cliente" {
+  source = "terraform-aws=modules/rds/aws"
+  identifier = "mysql-cliente"
+  engine = "mysql"
+  engine_version = "8.0"
+  instance_class = "db.t3.micro"
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = [module.subnets.private_subnet_ids[1]]
+  security_group_ids = [module.security_groups.db_security_group_id]
 }
