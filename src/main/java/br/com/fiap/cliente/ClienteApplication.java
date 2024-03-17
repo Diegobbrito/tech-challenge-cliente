c419cd335cfabc59486c4758856682fe0fe0a048
Diego Brito <dibbrito@gmail.com>
2024-03-17 17:13:56 -0300

    enable rabbit

--- src/main/java/br/com/fiap/cliente/ClienteApplication.java
+++ src/main/java/br/com/fiap/cliente/ClienteApplication.java
@@ -1,9 +1,11 @@
 package br.com.fiap.cliente;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class ClienteApplication {
 
 	public static void main(String[] args) {
